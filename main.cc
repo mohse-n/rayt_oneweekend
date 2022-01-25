95c47344f7a09a7e8e3ac0779907be9c9d539250
mohse-n <mohsen.alizadeh.noghani@gmail.com>
2022-01-25 12:31:45 -0500

    add anti-aliasing

--- main.cc
+++ main.cc
@@ -3,6 +3,7 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
 
@@ -46,6 +47,7 @@ int main(){
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -55,14 +57,7 @@ int main(){
     world.add(make_shared<sphere>(point3(0,-100.5,-1),100));
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio*viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0,0,0);
-    auto horizontal = vec3(viewport_width,0,0);
-    auto vertical = vec3(0,viewport_height,0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0,0,focal_length);
+    camera cam;
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -70,15 +65,19 @@ int main(){
     for (int j=image_height-1;j>=0;j--){
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i=0;i<image_width;i++){
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
-            /* Cast a ray to each pixel. 
-            The start of the ray is (0,0,0) or origin hence -origin term.
-            lower_left_corner+u*horizontal+v*vertical spans all the pixels. */
-            ray r(origin,lower_left_corner+u*horizontal+v*vertical-origin);
+            color pixel_color(0,0,0);
+            /* Cast rays around each pixel. */
+            for (int s=0;s<samples_per_pixel;s++){
+            auto u = (i+random_double()) / (image_width-1);
+            auto v = (j+random_double()) / (image_height-1);
+        
+            ray r = cam.get_ray(u,v);
             /* Calculate the color that we see. */
-            color pixel_color = ray_color(r,world);
-            write_color(std::cout,pixel_color);
+            pixel_color += ray_color(r,world);
+            }
+
+        write_color(std::cout,pixel_color, samples_per_pixel);
+        
         }
 
     }
